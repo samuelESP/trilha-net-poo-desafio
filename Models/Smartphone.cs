3ae51c493d37e125eccb84a35c09bc3648111334
Samuel Espíndola <62106502+samuelESP@users.noreply.github.com>
2025-11-02 19:19:23 -0300

    Entrega desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,12 +4,29 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        private string Modelo { get; }
+        private string IMEI { get; }
+        private int Memoria { get; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
+
+            if (string.IsNullOrWhiteSpace(imei))
+            {
+                throw new ArgumentNullException("O número IMEI não pode ser vazio");
+            }
+
+            if(imei.Length < 15)
+            {
+                throw new ArgumentNullException("O número IMEI não pode ser menor que 15");
+            }
+
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            this.Modelo = modelo;
+            this.IMEI = imei;
+            this.Memoria = memoria;
         }
+
 
         public void Ligar()
         {
@@ -22,5 +39,15 @@ namespace DesafioPOO.Models
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Dispositivo: {this.Modelo}");
+            Console.WriteLine($"Número: {this.Numero}");
+            Console.WriteLine($"IMEI: {this.IMEI}");
+            Console.WriteLine($"Memória: {this.Memoria} GB");
+            Console.WriteLine("------------------------------");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: \n");
+Smartphone nokia = new Nokia("123", " Nokia C20", "111111111111111", 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+nokia.ExibirDados();
+
+
+
+Console.WriteLine("Iphone: \n");
+Smartphone iphone = new Iphone("456", "16", "", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Twitch");
+iphone.ExibirDados();
+
